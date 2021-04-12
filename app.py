1cf786eaeaa1f92a53911e5e3c4628729667104c
Rohitkuru <rohit.kuru@gmail.com>
2021-04-12 12:09:17 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+
+
+## So this is my first python program
+
+### calculator
+
+
+def add():
+    result = first_no + second_no
+    print(result)
+
+def mul():
+    pass
+
+def div():
+    pass
+
+def sub():
+    pass
+
+
+if __name__ == '__main__':
+    first_no = 10
+    second_no = 20
+
+    # call addition
+    add()
+
+
+
+
+
